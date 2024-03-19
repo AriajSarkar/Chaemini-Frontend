92aeb1c697ee4ecde9c20ec42105965aea6a5475
Ariaj Sarkar <rajsarkar02205@gmail.com>
2024-03-19 21:08:17 +0530

    changes-fixes

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import dotenv from 'dotenv'
 
-dotenv.config()
-
-// Get the proxy URL from the environment variables
-const proxyUrl: string | undefined = process.env.PROXY_URL
-
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api/v1/g/': {
-        target: proxyUrl || undefined, // Use undefined if proxyUrl is undefined
-        changeOrigin: true,
-        // Add any other proxy options here if needed
-      }
+      '/api/v1/g/': 'https://chaemini-backend.vercel.app/',
     }
   }
 })
