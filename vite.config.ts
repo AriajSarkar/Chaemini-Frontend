f8b292d23d2cd51e99aafb351925d7780b1ff50d
Ariaj Sarkar <rajsarkar02205@gmail.com>
2024-03-25 19:57:44 +0530

    fix build func

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import http from 'http';
-import https from 'https';
-import { loadEnv } from 'vite';
 
-export default defineConfig(({ mode }) => {
-  // Load environment variables based on the current mode
-  const env = loadEnv(mode, process.cwd());
-
-  // Determine the agent based on the protocol of the target URL
-  const agent = env.VITE_API_BASE_URL.startsWith('https:') ?
-                new https.Agent({ rejectUnauthorized: false }) :
-                new http.Agent();
-
+export default defineConfig(() => {
   return {
     plugins: [react()],
     server: {
       proxy: {
         '/api/v1/g/': {
-          target: env.VITE_API_BASE_URL,
+          target: 'https://chaemini-backend.vercel.app',
           changeOrigin: true,
-          agent: agent,
+          // Add any other proxy options here if needed
         },
       },
     },
